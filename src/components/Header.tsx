2d4a0f5872bf033baef29160e6c9fd4ebc02338e
unknown <ckdduf138@naver.com>
2025-04-07 21:02:30 +0900

    seo: seo 최적화

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 const Header: React.FC = () => {
   return (
     <header className="bg-white p-4 sm:p-6">
-      <div className="container mx-auto flex items-center justify-center">
-        <h1 className="text-3xl sm:text-4xl font-bold text-center text-blue-400 tracking-wide">Swift</h1>
-        <h1 className="text-3xl sm:text-4xl font-bold text-center text-red-400 tracking-wide">Mind</h1>
+      <div className="container mx-auto flex justify-center">
+        <h1 className="text-3xl sm:text-4xl font-bold tracking-wide">
+          <span className="text-blue-400">Swift</span>
+          <span className="text-red-400">Mind</span>
+        </h1>
       </div>
     </header>
   );
